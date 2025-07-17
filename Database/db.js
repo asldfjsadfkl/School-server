140633401496e7bf2c5a363b32a019ebb7abe26b
asldfjsadfkl <mna45994445@gmail.com>
2025-07-17 06:38:47 -0700

    db-url

--- Database/db.js
+++ Database/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Database = () => {
   try {
-    // const DB_URL = "mongodb://127.0.0.1:27017/jamia"
-    const DB_URL = "mongodb+srv://root:root@cluster0.iarf5hy.mongodb.net/schoolmanagement"
+    const DB_URL = process.env.DB_URL;
+    console.log(DB_URL)
+    // "mongodb://127.0.0.1:27017/jamia"
+    // const DB_URL = "mongodb+srv://root:root@cluster0.iarf5hy.mongodb.net/schoolmanagement"
     mongoose.connect(DB_URL);
     console.log("DB Connected");
   } catch (error) {
